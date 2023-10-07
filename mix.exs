5bcf17066b98c8d02a12eb94859272d369c89c06
Ryan B. Harvey <rharvey@mbta.com>
2023-10-06 21:11:49 -0500

    Mix and git configuration files

--- mix.exs
+++ mix.exs
@@ -9,7 +9,9 @@ defmodule Phil.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: LcovEx, ignore_paths: ["test/", "deps/"]],
+      dialyzer: [plt_add_apps: [:mix]]
     ]
   end
 
@@ -17,9 +19,18 @@ defmodule Phil.MixProject do
   #
   # Type `mix help compile.app` for more information.
   def application do
+    apps = [:logger, :runtime_tools]
+
+    apps =
+      if Mix.env() == :prod do
+        [:ehmon, :diskusage_logger | apps]
+      else
+        apps
+      end
+
     [
       mod: {Phil.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: apps
     ]
   end
 
@@ -32,24 +43,36 @@ defmodule Phil.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:assert_html, "~> 0.1.3", only: :test},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:diskusage_logger, "~> 0.2.0", only: [:prod]},
+      {:ecto_sql, "~> 3.10"},
+      {:esbuild, "~> 0.7", runtime: Mix.env() == :dev},
+      {:finch, "~> 0.13"},
+      {:floki, ">= 0.30.0", only: :test},
+      {:gettext, "~> 0.20"},
+      {:jason, "~> 1.2"},
+      {:lcov_ex, "~> 0.3", only: [:dev, :test], runtime: false},
+      {:logster, "~> 1.1"},
+      {:mox, "1.1.0", only: :test},
       {:phoenix, "~> 1.7.7"},
       {:phoenix_ecto, "~> 4.4"},
-      {:ecto_sql, "~> 3.10"},
-      {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.3"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "~> 0.19.0"},
-      {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.0"},
-      {:esbuild, "~> 0.7", runtime: Mix.env() == :dev},
-      {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
+      {:plug_cowboy, "~> 2.5"},
+      {:postgrex, ">= 0.0.0"},
+      {:sobelow, "~> 0.11", only: [:dev, :test], runtime: false},
       {:swoosh, "~> 1.3"},
-      {:finch, "~> 0.13"},
+      {:tailwind, "~> 0.2.0", runtime: Mix.env() == :dev},
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
-      {:gettext, "~> 0.20"},
-      {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:timex, "3.7.11"},
+      {:typed_ecto_schema, "0.4.1"},
+      {:tzdata, "~> 1.1"},
+      {:uuid, "1.1.8"}
     ]
   end
 
@@ -61,10 +84,18 @@ defmodule Phil.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
-      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build", "phx.gen.cert"],
+      "ecto.setup": ["ecto.create", "ecto.load", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
+      "ecto.migrate": ["ecto.migrate", "ecto.dump"],
+      test: ["ecto.drop", "ecto.create --quiet", "ecto.load", "test"],
+      check: [
+        "format",
+        "compile --force --all-warnings",
+        "dialyzer --quiet",
+        "credo --strict",
+        "sobelow --skip --verbose --ignore Config.HTTPS"
+      ],
       "assets.setup": ["tailwind.install --if-missing", "esbuild.install --if-missing"],
       "assets.build": ["tailwind default", "esbuild default"],
       "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
