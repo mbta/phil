d3b22a4434b8459534a346daa5d255f917ef8bd6
Ryan B. Harvey <rharvey@mbta.com>
2023-10-06 21:13:10 -0500

    Add a structure.sql file

---
+++ priv/repo/structure.sql
@@ -0,0 +1,29 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 14.9
+-- Dumped by pg_dump version 15.4
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: public; Type: SCHEMA; Schema: -; Owner: -
+--
+
+-- *not* creating schema, since initdb creates it
+
+
+--
+-- PostgreSQL database dump complete
+--
+
