dbe2f32e95bbd375c4bc1d61efb7aec81308cff6
Ryan B. Harvey <rharvey@mbta.com>
2023-10-13 10:40:00 -0500

    Add to url for Endpoint config in runtime

--- config/runtime.exs
+++ config/runtime.exs
@@ -41,7 +41,7 @@ if config_env() == :prod do
   port = String.to_integer(System.get_env("PORT") || "4001")
 
   config :phil, PhilWeb.Endpoint,
-    url: [host: host],
+    url: [host: host, port: 443, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
