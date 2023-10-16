41c977ac938380ef1bfc59c5770a3e0b559749a7
Ryan B. Harvey <rharvey@mbta.com>
2023-10-16 09:33:32 -0500

    Remove stray IO.inspect

--- config/runtime.exs
+++ config/runtime.exs
@@ -17,7 +17,7 @@ import Config
 # Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
 # script that automatically sets the env var above.
 if System.get_env("PHX_SERVER") do
-  config :phil, PhilWeb.Endpoint, server: true |> IO.inspect(label: "Endpoint config: server")
+  config :phil, PhilWeb.Endpoint, server: true
 end
 
 if config_env() == :prod do
